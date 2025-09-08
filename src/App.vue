1009ffcf4b301a4aa319a5c862c46128b592d794
Wanfogai <wanfogai770@gmail.com>
2025-09-08 20:28:49 +0300

    Добавил Hero блок

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <script setup lang="ts">
-import { Icon, IconTypeEnum, UiButton } from "./components/ui";
-import { Header, SearchPanel,Container, Footer } from "./components/widgets";
+import {
+  Header,
+  SearchPanel,
+  Container,
+  Footer,
+  HeroBlock,
+} from "./components/widgets";
 </script>
 
 <template>
- <Container class="flex-col bg-[linear-gradient(180deg,rgba(0,0,0,1)_0%,rgba(25,0,246,1)_50%,rgba(0,0,0,1)_100%)]">
-
-
- 
-  <Container class=" bg-black justify-between" indent-top="20px" indent-left="10%" marginx-10>
-  <Header class="head"></Header>
+  <Container
+    class="flex-col bg-[linear-gradient(180deg,rgba(0,0,0,0.2)_0%,rgba(25,0,246,0.2)_50%,rgba(0,0,0,0.7)_100%)]"
+  >
+    <Header class="head"></Header>
+    <SearchPanel />
+    <HeroBlock />
+    <Footer />
   </Container>
-  <Container class=" justify-between" indent-top="20px" indent-left="10%" marginx-10>
-    <SearchPanel class="w-full"/>
-  </Container>
-    
-      <Footer/>
-    
-  
-   </Container>
 </template>
 
 <style scoped>
-.head{
+.head {
   margin-bottom: 20px;
 }
 </style>
