f856795672b520862d970e36b741f5032dbe8f72
Wanfogai <wanfogai770@gmail.com>
2025-09-08 03:55:50 +0300

    Добавил и настроил панель поиска

--- src/components/widgets/SearchPanel/searchPanel.vue
+++ src/components/widgets/SearchPanel/searchPanel.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { UISelect } from '../../ui';
+import { Icon, UISelect } from '../../ui';
 
 
 const categoryType = ref('Элементы');
+const options = [{value:'Elements',text:'Элементы'},{value:'humans',text:'Люди'},{value:'drugs',text:'Наротики'}]
 
 </script>
 <template>
-<div class="search-container">
-       <UISelect/>
+<div class="flex border-solid border-2 border-slate-700 rounded-2xl px-4 py-5 items-center">
+       <div class="w-25"><UISelect v-model="categoryType" :items="options" class="font-rubik text-lg"/></div>
+       <div class="flex border-solid border h-5 border-white rounded-2xl mx-5 opacity-20"/>
+       <div class="flex w-full">
+              <Icon icon="Search" class="mr-5"></Icon>
+              <input type="text" placeholder="Поиск" class="w-full font-rubik text-white bg-transparent border-none outline-none"/>
+       </div>
+       
 </div>
 </template>
 <style lang="scss" scoped>
