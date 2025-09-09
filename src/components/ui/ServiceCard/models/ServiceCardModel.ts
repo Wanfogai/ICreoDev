ab73c81622973e7133ed05caea8fe76c690ea4dd
Wanfogai <wanfogai770@gmail.com>
2025-09-09 19:30:31 +0300

    Добавил модель для карточек

---
+++ src/components/ui/ServiceCard/models/ServiceCardModel.ts
@@ -0,0 +1,18 @@
+export default class ServiceCardModel {
+  constructor(
+    bg: String,
+    text: String,
+    size: Number = 100,
+    autoWrap: boolean = false
+  ) {
+    this.background = bg;
+    this.text = text;
+    this.size = size;
+    this.autoWrap = autoWrap;
+  }
+  id: String;
+  background: String = "";
+  text: String = "";
+  size: Number = 50;
+  autoWrap: boolean = false;
+}
