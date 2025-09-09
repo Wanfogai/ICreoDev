6bbf645d7f3f51ff1793ef5c3ac4a0cd4902d727
Wanfogai <wanfogai770@gmail.com>
2025-09-09 19:30:03 +0300

    Добавил 2 контейнера с карточками

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ServiceCard } from "./components";
+import ServiceCardModel from "./components/ui/ServiceCard/models/ServiceCardModel";
 import {
   Header,
   SearchPanel,
@@ -6,15 +8,41 @@ import {
   Footer,
   HeroBlock,
 } from "./components/widgets";
+
+const row1 = [
+  new ServiceCardModel("LiveRec.jpg", "Живая съёмка", 100, true),
+  new ServiceCardModel("Gifs.jpg", "Гифки", 75, false),
+  new ServiceCardModel("DoneTemplate.jpg", "Готовые шаблоны", 150, true),
+];
+
+const row2 = [
+  new ServiceCardModel("Animations.jpg", "Анимации", 33, true),
+  new ServiceCardModel("Futa.jpg", "Футажи", 150, false),
+  new ServiceCardModel("Sounds.jpg", "Звуки", 33, true),
+];
 </script>
 
 <template>
   <Container
     class="flex-col bg-[linear-gradient(180deg,rgba(0,0,0,0.2)_0%,rgba(25,0,246,0.2)_50%,rgba(0,0,0,0.7)_100%)]"
   >
-    <Header class="head"></Header>
+    <Header class="head" />
     <SearchPanel />
-    <HeroBlock />
+    <HeroBlock class="mb-[60px]" />
+
+    <Container
+      indent-left="260px"
+      class="flex gap-4 w-full overflow-x-auto mb-[1%]"
+    >
+      <ServiceCard v-for="(card, i) in [...row1]" :key="i" :model="card" />
+    </Container>
+    <Container
+      indent-left="260px"
+      class="flex gap-4 w-full overflow-x-auto mb-[4%]"
+    >
+      <ServiceCard v-for="(card, i) in [...row2]" :key="i" :model="card" />
+    </Container>
+
     <Footer />
   </Container>
 </template>
