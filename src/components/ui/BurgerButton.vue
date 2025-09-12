874706aad2653cd54620c4f04f529162ae3b3148
Wanfogai <wanfogai770@gmail.com>
2025-09-12 20:07:17 +0300

    Изменил тип наложения

--- src/components/ui/BurgerButton.vue
+++ src/components/ui/BurgerButton.vue
@@ -8,7 +8,7 @@ const isOpen = ref(false);
   <!-- Кнопка-бургер -->
   <button
     @click="isOpen = !isOpen"
-    class="relative w-8 h-8 flex flex-col justify-between items-center p-1 z-50 md:hidden"
+    class="relative w-8 h-8 flex flex-col justify-between items-center p-1 z-20 md:hidden"
   >
     <!-- Линия 1 -->
     <span
@@ -38,13 +38,13 @@ const isOpen = ref(false);
   <!-- Выезжающее меню -->
   <transition name="slide">
     <div
-      v-if="isOpen"
-      class="fixed top-0 right-0 w-1/2 h-full bg-gradient-to-b from-purple-900 to-black shadow-xl z-40 flex flex-col"
+      v-show="isOpen"
+      class="fixed top-0 left-0 w-full h-full bg-gradient-to-b from-purple-900 to-black shadow-xl z-40 flex flex-col"
     >
       <!-- Кнопка-крестик внутри меню -->
       <button
         @click="isOpen = false"
-        class="self-end m-4 w-8 h-8 flex flex-col justify-between items-center"
+        class="self-end m-4 mt-[30px] w-8 h-8 flex flex-col justify-between items-center"
       >
         <span
           class="block w-7 h-0.5 bg-white rounded rotate-[45deg] translate-y-2"
@@ -72,7 +72,7 @@ const isOpen = ref(false);
   transition: all 0.3s ease;
 }
 .slide-enter-from {
-  transform: translateX(100%);
+  transform: translateX(-100%);
 }
 .slide-enter-to {
   transform: translateX(0);
@@ -81,6 +81,6 @@ const isOpen = ref(false);
   transform: translateX(0);
 }
 .slide-leave-to {
-  transform: translateX(100%);
+  transform: translateX(-100%);
 }
 </style>
