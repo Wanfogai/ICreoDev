ef29c1b81f6c1f1c40c55d6e48d6ab9961cbd54f
Wanfogai <wanfogai770@gmail.com>
2025-09-12 20:09:15 +0300

    Мобильная адоптация

--- src/components/widgets/SearchPanel/searchPanel.vue
+++ src/components/widgets/SearchPanel/searchPanel.vue
@@ -11,28 +11,27 @@ const options = [
 ];
 </script>
 <template>
-  <Container class="justify-between py-[20px] md:px-[260px]">
+  <Container
+    class="relative z-20 justify-between mt-[20px] md:mt-0 md:py-[20px] md:px-[260px]"
+  >
     <div
-      class="flex border-solid border-2 border-slate-700 rounded-2xl px-4 py-5 items-center w-full"
+      class="flex gap-2 border-solid md:border-2 rounded-b-[12px] border-[2px] md:border-slate-700 border-gray-900 md:rounded-2xl px-4 py-5 items-center w-full"
     >
-      <div>
-        <UISelect
-          v-model="categoryType"
-          :items="options"
-          class="font-rubik text-lg"
-        />
-      </div>
-      <div
-        class="flex border-solid border h-5 border-white rounded-2xl mx-5 opacity-20"
+      <UISelect
+        v-model="categoryType"
+        :items="options"
+        class="font-rubik text-lg order-4 md:order-1"
       />
-      <div class="flex w-full">
-        <Icon icon="Search" class="mr-5"></Icon>
-        <input
-          type="text"
-          placeholder="Поиск"
-          class="w-full font-rubik text-white bg-transparent border-none outline-none"
-        />
-      </div>
+
+      <div
+        class="flex border-solid order-3 md:order-2 border h-6 border-white rounded-2xl md:mx-5 md:mr-3 opacity-15"
+      />
+      <Icon icon="Search" class="md:mr-2 w-[30px] order-2 md:order-3"></Icon>
+      <input
+        type="text"
+        placeholder="Поиск"
+        class="w-full font-rubik order-1 md:order-4 text-white bg-transparent border-none outline-none"
+      />
     </div>
   </Container>
 </template>
